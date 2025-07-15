af7ef49c6a05800c791d78c08266bc735f81bc03
nikos <nickdereken@gmail.com>
2025-07-15 17:35:19 +0300

    added logout route and middleware in routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,8 +3,11 @@ const validateToken = require("../middleware/validateTokenHandler");
 const router = express.Router();
 const {
     loginUser,
+    logoutUser,
 } = require("../controllers/userController");
 
 router.post("/login", loginUser);
+router.use(validateToken);
+router.post("/logout", logoutUser);
 
 module.exports = router;
