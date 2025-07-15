442334863c0417a181ef39f7ad2c1db916f3e3b1
nikos <nickdereken@gmail.com>
2025-07-15 17:35:43 +0300

    changed tokenauth from bearer to cookie and also check if blacklisted

--- server/middleware/validateTokenHandler.js
+++ server/middleware/validateTokenHandler.js
@@ -1,21 +1,26 @@
 const asyncHandler = require("express-async-handler");
 const jwt = require("jsonwebtoken");
+const {blacklist} = require("../config/dbConnection");
 
 const validateToken = asyncHandler(async (req, res, next) => {
   let token;
   // ta epomena duo lines ta sizitame alla gia tora komple
-  let authHeader = req.headers.authorization || req.headers.Authorization;
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+  const authHeader = req.headers['cookie'];
+  if (!authHeader) {
     res.status(401);
     throw new Error("Token missing from request");
   }
-
-  token = authHeader.split(" ")[1];
+  const cookie = authHeader.split('=')[1]; // If there is, split the cookie string to get the actual jwt token
+  token = cookie.split(';')[0];
   if (!token) {
     res.status(401);
-    throw new Error("Token missing after Bearer");
+    throw new Error("Token missing from cookie");
   }
-  
+  const checkIfBlacklisted = await blacklist.findOne({ where: { token: token } });
+  if (checkIfBlacklisted){
+    res.status(401);
+    throw new Error("Not logged in");
+  }
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
     if (err){
       res.status(401);
@@ -23,6 +28,7 @@ const validateToken = asyncHandler(async (req, res, next) => {
     }
     // analoga me ti json tha kanoume sign
     req.user = decoded.user;
+    req.token = token;
     next(); 
   });
 });
