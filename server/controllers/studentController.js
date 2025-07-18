ced73a630b32b27e06e136f8eef05338205bf2ae
MitrogiannisV <vaggelismitrogiannis@icloud.com>
2025-07-18 13:12:00 +0300

    lathos se scomment - diorthosi

--- server/controllers/studentController.js
+++ server/controllers/studentController.js
@@ -4,7 +4,7 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 //@desc Get Thesis Info
-//@route Get /api/student/:id/thesis
+//@route Get /api/student/thesis
 //@access Private
 const getThesisInfo = asyncHandler(async (req, res) => {
   const loggedStudent = await student.findOne({ where: {student_userid: req.user.id} });
