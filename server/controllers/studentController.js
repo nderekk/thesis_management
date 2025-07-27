4202bb72859f11c1ff7ca6d41ae041837803a7eb
nikos <nickdereken@gmail.com>
2025-07-27 16:13:06 +0300

    ekana mlkia evaza body se get prepei na ta pernas ws query

--- server/controllers/studentController.js
+++ server/controllers/studentController.js
@@ -16,7 +16,7 @@ const getThesisInfo = asyncHandler(async (req, res) => {
     const loggedStudent = await student.findOne({ where: {student_userid: req.user.id} });
     studentThesis = await thesis.findOne({ where: {student_am: loggedStudent.am}});
   } else {
-    studentThesis = await thesis.findOne({ where: {id: req.body.id}});
+    studentThesis = await thesis.findOne({ where: {id: req.query.id}});
   }
   const thesisTopic = await thesis_topics.findOne({ where: {id: studentThesis.topic_id}});
   const supervisor = await professor.findOne({ where: {am: studentThesis.supervisor_am}});
