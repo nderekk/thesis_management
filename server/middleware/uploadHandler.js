3a4d58863b8d4538876bedeab78496f01310cdb7
Nikos <nickdereken@gmail.com>
2025-09-04 00:25:13 +0300

    server side error handling for wrong file types/too large file sizes

--- server/middleware/uploadHandler.js
+++ server/middleware/uploadHandler.js
@@ -13,6 +13,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({
   storage,
+  limits: { fileSize: 10 * 1024 * 1024 }, // 10 mb max
   fileFilter: (req, file, cb) => {
     if (file.mimetype === 'application/pdf') {
       cb(null, true);
